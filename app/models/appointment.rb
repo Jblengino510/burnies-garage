6514a17635ba9b304c0a6358865705a30b9fe23a
Joshua <j.blengino@yahoo.com>
2022-02-14 17:04:44 -0800

    basic validations and associations added to models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :service
+
+  validates :date_requested, :firstname, :lastname, :phone_number, :city, :vehicle_make, :vehicle_model, :vehicle_year, :vehicle_color, presence: true
+  validates :phone_number, :vehicle_year, numericality: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, uniqueness: true, length: { minimum: 5 }
+    validates :password, presence: true, length: { in: 6..30 }, confirmation: true, on: :create
+    validates :password_confirmation, presence: true, on: :create
+
+    has_many :appointments, dependent: :destroy
+    has_many :services, through: :appointments
 end
